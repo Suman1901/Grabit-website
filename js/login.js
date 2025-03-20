ee7ecf3a4f169f865c1f8a7ea0f3d6c95587c4d4
Suman1901 <sumansahoo41243@gmail.com>
2025-03-20 18:03:56 +0530

    Add login js file, comment box update and some style update

---
+++ js/login.js
@@ -0,0 +1,65 @@
+
+const loginBtn= document.querySelector("#login-btn")
+
+
+// loginBtn.addEventListener("click", (eve)=>{
+//     eve.preventDefault();
+
+//     const email = document.querySelector("#emailInput").value.trim();
+//     const password = document.querySelector("#passInput").value;
+
+
+    // switch (true){
+    //     case email === '':
+    //         const emailError = document.querySelector("#email-error");
+    //         emailError.innerHTML = "Enter your email address";
+
+    //         break;
+    //     case password === '':
+    //         const passwordError = document.querySelector("#password-error");
+    //         passwordError.innerHTML = "Enter your password";
+            
+    //         break;
+    //     default:
+            
+    // }
+
+ 
+// })
+
+
+function validateForm(){
+    const email = document.querySelector("#emailInput").value.trim();
+    const password = document.querySelector("#passInput").value;
+    const emailError = document.querySelector("#email-error");
+    const passError = document.querySelector("#password-error");
+
+    let validForm = true;
+
+    if(email === ""){
+        emailError.innerHTML = "Enter your email address";
+        validForm = false;
+    }else if (email.length < 10){
+        emailError.innerHTML = "email address must be at least 10 characters long";
+        validForm = false;
+    } else{
+        emailError.innerHTML = "";
+    }
+
+    if (password === ""){
+        passError.innerHTML = "Enter your password";
+        validForm = false;
+    }else if (password.length < 6 || !/[A-Z]/.test(password) || !/[!@#$%^&*]/.test(password)){
+        passError.innerHTML = "password required min 6 caracters and include one capital letter and one special character";
+        validForm = false;
+    }else {
+        passError.innerHTML = "";
+    }
+
+    if (validForm) {
+        window.href = "index.html"; // Replace with your desired page
+    }
+
+    return validForm;
+}
+
